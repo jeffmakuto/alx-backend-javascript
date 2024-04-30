4e764b674901b18062f8a053d078cbb2aa7af9f4
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-04-30 16:52:32 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,16 +4,15 @@
  * 
  * @module stdin
  */
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on('data', function(data) {
+    const input = data.trim();
+    if (input) {
+        process.stdout.write(`Your name is: ${input}\n`);
+        process.stdout.write('This important software is now closing\n');
+        process.exit();
+    }
 });
